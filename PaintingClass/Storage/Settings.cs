87265f2748ba6a0d2eeefabd45baf9c086a9193f
RocketPrinter <calinioncioaia@gmail.com>
2021-02-22 16:23:25 +0200

    Storage API si trecut la .NET 5

---
+++ PaintingClass/Storage/Settings.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace PaintingClass.Storage
+{
+    [Serializable]
+    public class Settings
+    {
+
+        void Save()
+        {
+            AppdataIO.Save<Settings>("GlobalSettings.xml", this);
+        }
+    }
+}
