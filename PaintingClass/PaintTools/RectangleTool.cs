9a536e3654dac6c865a055256f27d4a091998eac
Gabriel <75862004+Gabriel1961@users.noreply.github.com>
2021-03-27 00:10:26 +0200

    Dreptunghi + patrat(shift) Tool

---
+++ PaintingClass/PaintTools/RectangleTool.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace PaintingClass.PaintTools
+{
+    /// <summary>
+    /// de desenat dreptunghiuri
+    /// </summary>
+    class RectangleTool : PaintTool
+    {
+
+        public override int priority => 0;
+
+        public override Control GetControl()
+        {
+            Label label = new Label();
+            label.Content = "Rectangle";
+            return label;
+        }
+
+
+        RectangleGeometry rectangle;
+        Point initialPos;
+        bool isControlPressed = false;
+        public override void MouseDown(Point position)
+        {
+            Window.GetWindow(whiteboard).KeyDown += Whiteboard_KeyDown;
+            Window.GetWindow(whiteboard).KeyUp += Whiteboard_KeyUp;
+            initialPos = position;
+            rectangle = new RectangleGeometry(new Rect(position,position));
+
+            whiteboard.collection.Add(new GeometryDrawing(null, new Pen(new SolidColorBrush(Colors.Black),0.3),rectangle));
+        }
+
+        private void Whiteboard_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftShift)
+                isControlPressed = true;
+        }
+
+        private void Whiteboard_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftShift)
+                isControlPressed = false;
+        }
+
+        public override void MouseDrag(Point position)
+        {
+            //patrat
+            if (isControlPressed)
+            {
+                double normx = position.X - initialPos.X;
+                double normy = (double)whiteboard.Height / whiteboard.Width*(position.Y - initialPos.Y);
+                if (Math.Abs(normy) > Math.Abs(normx)) 
+                {//x
+                    Window.GetWindow(whiteboard).Title = $"{normx} {normy}";
+                    if (initialPos.Y - position.Y < 0)
+                        rectangle.Rect = new Rect(initialPos, new Point(position.X, (initialPos.Y + (double)whiteboard.Width / whiteboard.Height * Math.Abs(position.X - initialPos.X))));
+                    else
+                        rectangle.Rect = new Rect(initialPos, new Point(position.X, (initialPos.Y - (double)whiteboard.Width / whiteboard.Height * Math.Abs(position.X - initialPos.X))));
+                }
+                else
+                {//y
+                    Window.GetWindow(whiteboard).Title = $"{normx} {normy}";
+                    if (initialPos.X - position.X < 0)
+                        rectangle.Rect = new Rect(initialPos, new Point(initialPos.X + (double)whiteboard.Height / whiteboard.Width * Math.Abs(position.Y - initialPos.Y), position.Y));
+                    else
+                        rectangle.Rect = new Rect(initialPos, new Point(initialPos.X - (double)whiteboard.Height / whiteboard.Width * Math.Abs(position.Y - initialPos.Y), position.Y));
+                }
+            }
+            else // dreptunghi
+                rectangle.Rect = new Rect(initialPos, position);
+        }
+
+        public override void MouseUp()
+        {
+            rectangle.Freeze();//extra performanta
+            rectangle = null;
+        }
+    }
+}
