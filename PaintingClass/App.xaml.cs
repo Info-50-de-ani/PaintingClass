6d95ab452ccd9efaa555bec4e0d503c8f0f511b3
RocketPrinter <calinioncioaia@gmail.com>
2021-05-04 17:27:56 +0300

    deschiderea prin protocol e gata acum

--- PaintingClass/App.xaml.cs
+++ PaintingClass/App.xaml.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using PaintingClass.Login;
+using System.Security.Principal;
+using System.Reflection;
 
 namespace PaintingClass
 {
@@ -20,10 +22,8 @@ namespace PaintingClass
 
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			return; // todo: temporar
-#pragma warning disable CS0162 // Unreachable code detected
+			//check for roomId
             if (e.Args.Length > 0)
-#pragma warning restore CS0162 // Unreachable code detected
             {
 				#region extragem roomId din event args
 				{
@@ -41,6 +41,71 @@ namespace PaintingClass
 
 				Debug.Assert(startedAppFromBrowserroomId != 0);
 			}
+
+			using var process = Process.GetCurrentProcess();
+			string path = process.MainModule.FileName + " \"%1\"";
+
+			//verificam daca cheia a fost adaugata
+			if (CheckKey(path) == true) 
+				return;
+			
+			if (IsAdministrator)
+            {
+				//scriem cheia
+				WriteKey(path);
+            }
+			else
+            {
+				string msg = "Pentru a termina instalarea aplicației sunt necesare drepturi de administrator. Doriți să restartați aplicația în modul de administrator?";
+				if (MessageBox.Show(msg, "PaintingClass",MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+					Elevate();
+			}
 		}
+
+		bool CheckKey(string path)
+		{
+			var key = Registry.ClassesRoot.OpenSubKey(Networking.Constants.customProtocol, false)
+				?.OpenSubKey("shell", false)
+				?.OpenSubKey("open", false)
+				?.OpenSubKey("command", false);
+
+			return key != null && key.GetValue("") as string == path;
+		}
+
+		void WriteKey(string path)
+        {
+			var key = Registry.ClassesRoot.CreateSubKey(Networking.Constants.customProtocol, true)
+				?.CreateSubKey("shell", true)
+				?.CreateSubKey("open", true)
+				?.CreateSubKey("command", true);
+			Debug.Assert(key != null);
+
+			key.SetValue("", path);
+		}
+
+		void Elevate()
+        {
+			ProcessStartInfo proc = new ProcessStartInfo()
+            {
+				UseShellExecute=true,
+				WorkingDirectory=Environment.CurrentDirectory,
+				FileName= Process.GetCurrentProcess().MainModule.FileName,
+				Verb = "runas"
+            };
+
+			try
+			{
+				Process.Start(proc);
+			}
+			catch (Exception e)
+			{
+				// The user refused the elevation
+				Trace.WriteLine(e.Message);
+				return;
+			}
+			Application.Current.Shutdown();
+        }
+
+		bool IsAdministrator =>new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
 	}
 }
